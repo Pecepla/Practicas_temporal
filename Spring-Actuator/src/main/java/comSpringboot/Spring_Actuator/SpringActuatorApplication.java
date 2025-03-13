bb07ec1226004200e484ee06c398f55cf39fd9fc
Pecepla <sectores.xvii@gmail.com>
2025-03-13 12:37:56 +0100

    Crso Udemy1

---
+++ Spring-Actuator/src/main/java/comSpringboot/Spring_Actuator/SpringActuatorApplication.java
@@ -0,0 +1,13 @@
+package comSpringboot.Spring_Actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringActuatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringActuatorApplication.class, args);
+	}
+
+}
